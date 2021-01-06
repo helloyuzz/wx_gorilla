2db3286ac12bd33b6bf6a957b83c972ba3991a3d
helloyuzz <39078847+helloyuzz@users.noreply.github.com>
2021-01-06 18:13:23 +0800

    Bootstrap Layout

--- wx_gorilla/wwwroot/js/site.js
+++ wx_gorilla/wwwroot/js/site.js
@@ -2,3 +2,32 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+function changeTheme(themeUrl) {
+    $('#mainWindow').css('background-image', "url('" + themeUrl + "')");
+    $('#openSidebarMenu').click();
+
+    $('#ChangeShowPanelIframe').attr('src', '/HidePanelPage?themeUrl=' + themeUrl);
+    $('#ChangeShowPanelIframe').reload();
+}
+function changeStyle(objid, type) {
+    $('#' + objid).attr('class', type);
+}
+function SwitchPanel() {
+    var showPanel = $('#hidden_ShowPanel').val();
+    if (showPanel.indexOf("none") != -1) {  // "display:none;"
+        $('#panelMenu').show();
+        $('#hidden_ShowPanel').val('block');
+        $('#ChangeShowPanelIframe').attr('src', '/HidePanelPage?showPanel=block');
+        $('#ChangeShowPanelIframe').reload();
+    } else {
+        $('#panelMenu').hide();
+        $('#hidden_ShowPanel').val('none');
+        $('#ChangeShowPanelIframe').attr('src', '/HidePanelPage?showPanel=none');
+        $('#ChangeShowPanelIframe').reload();
+    }
+}
+function Init() {
+    $('#ChangeShowPanelIframe').attr('src', '');
+    //$('#ChangeShowPanelIframe').reload();
+}
+onload = Init;
